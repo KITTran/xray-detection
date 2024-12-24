24ef8c1184ec517e92d2cc35e915e8c14f7abd29
KITTran <tuankiettuankha@gmail.com>
2024-12-24 17:56:05 +0900

    Update train

--- models/train.py
+++ models/train.py
@@ -6,6 +6,23 @@ import numpy as np
 import torch
 import torch.nn as nn
 import torch.optim as optim
-from torch.utils.data import DataLoader
 
-from modules.losses import make_one_hot, BinaryDiceLoss
+import albumentations as A
+from ..modules import utils, models, losses
+
+config = {
+    'IMG_SIZE': (256, 256),
+    'DATA_DIR': 'data/gdxray/welding/W0001',
+    'LABEL_DIR': 'data/gdxray/welding/W0002',
+    'BATCH_SIZE': 32,
+    'EPOCHS': 10,
+    'LR': 0.001,
+    'DEVICE': 'cuda' if torch.cuda.is_available() else 'cpu'
+}
+
+transform = A.Compose([
+    A.RandomBrightnessContrast(p=0.2),
+    A.Normalize()
+])
+
+dataset = utils.GDXrayDataset(config, "train", transform=transform, )
