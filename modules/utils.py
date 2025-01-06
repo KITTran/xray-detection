cc956a6996def41fb6c6932de3317b4731bd600f
KITTran <tuankiettuankha@gmail.com>
2025-01-06 16:52:28 +0900

    Reconfig dataset dir and function

--- modules/utils.py
+++ modules/utils.py
@@ -1,13 +1,13 @@
-from cProfile import label
-import json
-import math
 import os
-import pathlib
+import math
+import random
 
 import numpy as np
-import torch
+import albumentations as A
+import matplotlib.pyplot as plt
+
 from torch.utils.data import Dataset
-from torchvision.io import read_image
+from torchvision import io
 
 CURRENT_DIR = os.path.dirname(os.path.realpath(__file__))
 
@@ -20,11 +20,13 @@ OBJECT_CLASSES = [BACKGROUND_CLASS, WELDING_DEFECT]
 # Don't use this class directly. Instead, sub-class it and override
 # the configurations you need to change.
 
+
 class Config(object):
     """Base configuration class. For custom configurations, create a
     sub-class that inherits from this one and override properties
     that need to be changed.
     """
+
     # Name the configurations. For example, 'COCO', 'Experiment 3', ...etc.
     # Useful if your code needs to do things differently depending on which
     # experiment is running.
@@ -155,14 +157,18 @@ class Config(object):
         self.BATCH_SIZE = self.IMAGES_PER_GPU * self.GPU_COUNT
 
         # Input image size
-        self.IMAGE_SHAPE = np.array(
-            [self.IMAGE_MAX_DIM, self.IMAGE_MAX_DIM, 3])
+        self.IMAGE_SHAPE = np.array([self.IMAGE_MAX_DIM, self.IMAGE_MAX_DIM, 3])
 
         # Compute backbone size from input image size
         self.BACKBONE_SHAPES = np.array(
-            [[int(math.ceil(self.IMAGE_SHAPE[0] / stride)),
-              int(math.ceil(self.IMAGE_SHAPE[1] / stride))]
-             for stride in self.BACKBONE_STRIDES])
+            [
+                [
+                    int(math.ceil(self.IMAGE_SHAPE[0] / stride)),
+                    int(math.ceil(self.IMAGE_SHAPE[1] / stride)),
+                ]
+                for stride in self.BACKBONE_STRIDES
+            ]
+        )
 
     def display(self):
         """Display Configuration values."""
@@ -172,6 +178,7 @@ class Config(object):
                 print("{:30} {}".format(a, getattr(self, a)))
         print("\n")
 
+
 class GDXrayDataset(Dataset):
     """
     Dataset of Xray Images
@@ -180,7 +187,7 @@ class GDXrayDataset(Dataset):
     An example image_id is: "Weldings/W0001/W0001_0004.png"
     """
 
-    def __init__(self, config : dict, labels : bool, transform):
+    def __init__(self, config: dict, labels: bool, transform):
         super().__init__()
         """
         Args:
@@ -201,49 +208,85 @@ class GDXrayDataset(Dataset):
 
         # Initialize the dataset infos
         self.image_info = []
-        self.image_ids = []
         self.image_indices = {}
-        self.label_ids = []
         self.class_info = [{"source": "", "id": 0, "name": "BG"}]
 
         # Add classes
-        self.add_class(config['name'], 1, "Defect")
+        self.add_class(config["name"], 1, "Defect")
 
         # Load the dataset
-        metadata = "../metadata/{}_{}.txt".format(config['name'], config['subset'])
+        metadata_img = "../metadata/{}_{}.txt".format(config["name"], "images")
+        metadata_label = "../metadata/{}_{}.txt".format(config["name"], "labels")
 
         # Load image ids from key 'image' in dictionary in metadata file
-        self.image_ids.extend(self.load_metadata(metadata, 'image'))
+        image_ids = []
+        image_ids.extend(self.load_metadata(metadata_img, "image"))
         if self.labels:
-            self.label_ids.extend(self.load_metadata(metadata, 'label'))
+            label_ids = []
+            label_ids.extend(self.load_metadata(metadata_label, "label"))
 
-        for i, image_id in enumerate(self.image_ids):
-            img_path = os.path.join(config['data_dir'], image_id)
+        for i, image_id in enumerate(image_ids):
+            img_path = os.path.join(config["data_dir"], image_id)
+            label_path = ""
             if self.labels:
-                label_path = os.path.join(config['data_dir'], self.labels_ids[i])
+                label_path = os.path.join(config["data_dir"], label_ids[i])
 
                 if not os.path.exists(label_path):
-                    print("Skipping ",image_id," Reason: No mask")
+                    print("Skipping ", image_id, " Reason: No mask")
+
+                    del self.image_ids[i]
+                    del self.label_ids[i]
+
                     continue
 
+            print("Adding image: ", image_id)
 
+            self.add_image(config["name"], image_id, img_path, label=label_path)
 
     def __len__(self):
-        return len(self.image_ids)
+        return len(self.image_indices)
+
+    def __getitem__(self, idx):
+        """
+        Args:
+            idx (int): Index of the data to fetch.
+
+        Returns:
+            tuple: (image, label) where both are transformed.
+        """
+
+        image = io.read_image(self.image_info[idx]["path"], io.ImageReadMode.RGB)
+        channel, height, width = image.shape
+        self.update_info(idx, height_org=height, width_org=width)
+
+        if self.labels:
+            label = io.read_image(self.image_info[idx]["label"], io.ImageReadMode.GRAY)
+            if self.transform:
+                image = self.transform(image)
+                label = self.transform(label)
+
+            return image, label
+
+        if self.transform:
+            image = self.transform(image)
+
+        return image
 
     def add_class(self, source, class_id, class_name):
         assert "." not in source, "Source name cannot contain a dot"
         # Does the class exist already?
         for info in self.class_info:
-            if info['source'] == source and info["id"] == class_id:
+            if info["source"] == source and info["id"] == class_id:
                 # source.class_id combination already available, skip
                 return
         # Add the class
-        self.class_info.append({
-            "source": source,
-            "id": class_id,
-            "name": class_name,
-        })
+        self.class_info.append(
+            {
+                "source": source,
+                "id": class_id,
+                "name": class_name,
+            }
+        )
 
     def add_image(self, source, image_id, path, **kwargs):
         image_info = {
@@ -254,6 +297,11 @@ class GDXrayDataset(Dataset):
         image_info.update(kwargs)
         self.image_info.append(image_info)
         self.image_indices[image_id] = len(self.image_info) - 1
+
+    def update_info(self, image_id, **kwargs):
+        info = self.image_info[image_id]
+        info.update(kwargs)
+        self.image_info[image_id] = info
 
     def load_metadata(self, metadata, key):
         """
@@ -268,19 +316,70 @@ class GDXrayDataset(Dataset):
             key (str): Key to load from the metadata file
         """
 
-        with open(metadata, 'r') as file:
-            data = json.load(file)
-        return data[key]
+        image_ids = []
+        with open(metadata, "r") as metadata_file:
+            image_ids += metadata_file.readlines()
+        return [p.rstrip() for p in image_ids]
+
+
+def visualize_samples(dataset, num_samples=3, labels=True):
+    """
+    Visualize random samples from the dataset with images in the first column and labels in the second.
+
+    Args:
+        dataset (Dataset): The PyTorch Dataset to visualize.
+        num_samples (int): Number of random samples to visualize.
+    """
+    # Randomly select indices
+    indices = random.sample(range(len(dataset)), num_samples)
+
+    # Set up the Matplotlib figure
+    fig, axes = plt.subplots(num_samples, 2, figsize=(10, 5 * num_samples))
+    if num_samples == 1:
+        axes = [axes]  # Ensure axes is iterable for a single sample
+
+    for i, idx in enumerate(indices):
+        image, label = dataset[idx]
+        image_path = dataset.image_info[idx]["path"]
+        label_path = dataset.image_info[idx]["label"]
+
+        # Convert tensors to NumPy arrays for visualization
+        image = image.permute(1, 2, 0).numpy()
+        label = label.permute(1, 2, 0).squeeze().numpy()
+
+        # Display the image
+        axes[i][0].imshow(image)
+        axes[i][0].set_title(
+            f"Image: {image_path.split('/')[-1].split('.')[0]} -- Size: {image.shape}"
+        )
+        axes[i][0].axis("off")
+
+        # Display the label
+        axes[i][1].imshow(label, cmap="gray")
+        axes[i][1].set_title(
+            f"Label: {label_path.split('/')[-1].split('.')[0]} -- Size: {label.shape}"
+        )
+        axes[i][1].axis("off")
+
+    plt.tight_layout()
+    plt.show()
+
 
 # Path: datasets/gdxray
 if __name__ == "__main__":
     config = {
-        'name': 'gdxray',
-        'data_dir': 'data/gdxray',
-        'subset': 'train',
-        'metadata': 'metadata',
-        'img_size': (512, 512)
+        "name": "gdxray",
+        "data_dir": os.path.join(os.path.dirname(CURRENT_DIR), "data/gdxray"),
+        "subset": "train",
+        "metadata": "metadata",
+        "img_size": (512, 512),
     }
 
-    dataset = GDXrayDataset(config, labels=True, transform=None)
-    print(dataset.image_ids)
+    transform = A.Compose([A.Resize(224, 224), A.RandomRotate90(0.5)])
+
+    dataset = GDXrayDataset(config, labels=True, transform=transform)
+
+    print("Number of images: ", len(dataset))
+
+    # Visualize
+    visualize_samples(dataset, num_samples=3)
