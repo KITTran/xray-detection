c2a1dd233ba1325e17c137c7a9dd7222789380bd
KITTran <tuankiettuankha@gmail.com>
2025-01-07 18:26:15 +0900

    Create augmentation function

---
+++ modules/augmented.py
@@ -0,0 +1,31 @@
+import albumentations as A
+from torchvision.transforms import v2
+
+# Define the transformations as functions
+
+def albumentations_transform(IMG_HEIGHT, IMG_WIDTH):
+    al_transform = A.Compose([
+        A.Resize(IMG_HEIGHT, IMG_WIDTH),
+        A.HorizontalFlip(p=0.5),
+        A.VerticalFlip(p=0.5),
+        A.RandomRotate90(p=0.5),
+        A.Transpose(p=0.5),
+        A.ShiftScaleRotate(p=0.5),
+        A.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
+        A.pytorch.transforms.ToTensorV2()
+    ])
+
+    return al_transform
+
+def torch_transform(IMG_HEIGHT, IMG_WIDTH):
+    tv_transform = v2.Compose([
+        v2.ToImage(),
+        v2.Resize((IMG_HEIGHT, IMG_WIDTH)),
+        v2.RandomHorizontalFlip(),
+        v2.RandomVerticalFlip(),
+        v2.RandomRotation(90),
+        v2.RandomAffine(0, translate=(0.1, 0.1)),
+        v2.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
+    ])
+
+    return tv_transform
