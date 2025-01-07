b440e325221de59621b26904b79de0bf1aa2ac0a
KITTran <tuankiettuankha@gmail.com>
2025-01-07 18:26:50 +0900

    Delete old train runner

--- models/train.py
+++ models/train.py
@@ -1,76 +1,31 @@
 import os
 import sys
-import argparse
-import numpy as np
 
 import torch
-import torch.nn as nn
 import torch.optim as optim
 
 import albumentations as A
-from torchvision import transforms
-
 import segmentation_models_pytorch as smp
 
 sys.path.append(os.path.abspath('..'))
-from modules import utils, models, losses, test
+from modules import utils, models, losses
+
+CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
+PARENT_DIR = os.path.dirname(CURRENT_DIR)
+
 config = {
-    'IMG_SIZE': (256, 256),
-    'DATA_DIR': 'data/gdxray/welding/W0001',
-    'LABEL_DIR': 'data/gdxray/welding/W0002',
-    'BATCH_SIZE': 32,
-    'EPOCHS': 10,
-    'LR': 0.001,
-    'DEVICE': 'cuda' if torch.cuda.is_available() else 'cpu'
-}
+        'name': "gdxray",
+        'data_dir': os.path.join(PARENT_DIR, "data/gdxray"),
+        'metadata': os.path.join(PARENT_DIR, "metadata"),
+        'subset': "train",
+        'labels': True,
+   }
 
-datadir = "/home/tuank/projects/cracked-detection/dataset/gdxray/welding"
+config = utils.Config
 
-# Define transforms
-transform = transforms.Compose([
-    transforms.Resize((256, 256)),  # Resize images to 128x128
-    transforms.ToTensor(),         # Convert images to PyTorch tensors
-])
+dataset = utils.GDXrayDataset(config, labels=config['labels'], transform=None)
 
-# Create dataset instance
-dataset = test.ImageLabelDataset(datadir, transform=transform)
+device = "cuda" if torch.cuda.is_available() else "cpu"
 
-# Get a sample
-img, lbl = dataset[0]
-print(f"Image shape: {img.shape}, Label shape: {lbl.shape}")
-
-# Create a DataLoader
-dataloader = torch.utils.data.DataLoader(dataset, batch_size=32, shuffle=True)
-
-# initialize the model
-output_list = [64, 128, 256, 512, 1024]
-num_parallel = 2
-upsampling_cfg = dict(type='carafe', scale_factor=2, kernel_up=5, kernel_encoder=3)
-
-model = models.WResHDC_FF(output_list, num_parallel, upsampling_cfg)
-
-# Define the loss function
-criterion = smp.losses.DiceLoss(mode='binary')
-
-# Define the optimizer
-optimizer = optim.Adam(model.parameters(), lr=0.001)
-
-# Train the model
-
-for epoch in range(10):
-    model.train()
-    for i, (images, labels) in enumerate(dataloader):
-        images = images.to(config['DEVICE'])
-        labels = labels.to(config['DEVICE'])
-
-        optimizer.zero_grad()
-        outputs = model(images)
-        loss = criterion(outputs, labels)
-        loss.backward()
-        optimizer.step()
-
-        if i % 10 == 0:
-            print(f"Epoch: {epoch}, Iteration: {i}, Loss: {loss.item()}")
-
-# Save the model
-torch.save(model.state_dict(), 'model.pth')
+if device == "cuda":
+    num_workers = torch.cuda.device_count() * 4
