4342df66c8303ffdc62e2833a10116f98e8e6ed0
KITTran <tuankiettuankha@gmail.com>
2025-01-08 18:12:36 +0900

    Delete dice loss due to error and add another dice loss

--- modules/losses.py
+++ modules/losses.py
@@ -99,40 +99,30 @@ class DiceLoss(nn.Module):
 
         return total_loss/target.shape[1]
 
-def dice_loss(output, target, weights=None, ignore_index=None):
-    """
-    output : NxCxHxW Variable
-    target :  NxHxW LongTensor
-    weights : C FloatTensor
-    ignore_index : int index to ignore from loss
-    """
-    eps = 0.0001
+def dice_coeff(input: torch,Tensor, target: torch.Tensor, reduce_batch_first: bool = False, epsilon: float = 1e-6):
+    # Average of Dice coefficient for all batches, or for a single mask
+    assert input.size() == target.size()
+    assert input.dim() == 3 or not reduce_batch_first
 
-    output = output.exp()
-    encoded_target = output.detach() * 0
-    if ignore_index is not None:
-        mask = target == ignore_index
-        target = target.clone()
-        target[mask] = 0
-        encoded_target.scatter_(1, target.unsqueeze(1), 1)
-        mask = mask.unsqueeze(1).expand_as(encoded_target)
-        encoded_target[mask] = 0
-    else:
-        encoded_target.scatter_(1, target.unsqueeze(1), 1)
+    sum_dim = (-1, -2) if input.dim() == 2 or not reduce_batch_first else (-1, -2, -3)
 
-    if weights is None:
-        weights = 1
+    inter = 2 * (input * target).sum(dim=sum_dim)
+    sets_sum = input.sum(dim=sum_dim) + target.sum(dim=sum_dim)
+    sets_sum = torch.where(sets_sum == 0, inter, sets_sum)
 
-    intersection = output * encoded_target
-    numerator = 2 * intersection.sum(0).sum(1).sum(1)
-    denominator = output + encoded_target
+    dice = (inter + epsilon) / (sets_sum + epsilon)
+    return dice.mean()
 
-    if ignore_index is not None:
-        denominator[mask] = 0
-    denominator = denominator.sum(0).sum(1).sum(1) + eps
-    loss_per_channel = weights * (1 - (numerator / denominator))
 
-    return loss_per_channel.sum() / output.size(1)
+def multiclass_dice_coeff(input: torch.Tensor, target: torch.Tensor, reduce_batch_first: bool = False, epsilon: float = 1e-6):
+    # Average of Dice coefficient for all classes
+    return dice_coeff(input.flatten(0, 1), target.flatten(0, 1), reduce_batch_first, epsilon)
+
+
+def dice_loss(input: torch.Tensor, target: torch.Tensor, multiclass: bool = False):
+    # Dice loss (objective to minimize) between 0 and 1
+    fn = multiclass_dice_coeff if multiclass else dice_coeff
+    return 1 - fn(input, target, reduce_batch_first=True)
 
 if __name__ == "__main__":
 
