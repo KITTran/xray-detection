0d8acdb16bf91c187a7a55f40897644210bdaed6
KITTran <tuankiettuankha@gmail.com>
2025-01-09 18:10:04 +0900

    Delete albumentation and use only torchvision augumentation

--- modules/augmented.py
+++ modules/augmented.py
@@ -1,60 +1,42 @@
-import albumentations as A
-from albumentations.pytorch import ToTensorV2
+%load_ext autoreload
+%autoreload 2
+
+from random import sample
 from torchvision.transforms import v2
+from torchvision.transforms.functional import InterpolationMode
 
 import utils
 import torch
 
 # Define the transformations as functions
-# TODO: FIX ToTensorV2 disapear label channel
 
-def albumentations_transform_train(IMG_SIZE):
-    al_transform = A.Compose([
-        A.Resize(IMG_SIZE[0], IMG_SIZE[1]),
-        A.HorizontalFlip(p=0.5),
-        A.VerticalFlip(p=0.5),
-        A.RandomRotate90(p=0.5),
-        A.Transpose(p=0.5),
-        A.ShiftScaleRotate(p=0.5),
-        A.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
-        # ToTensorV2(),
-    ])
-
-    return al_transform
-
-def torch_transform_train(IMG_SIZE):
-    tv_transform = v2.Compose([
-        v2.ToImage(),
-        v2.Resize(IMG_SIZE),
-        v2.RandomHorizontalFlip(),
-        v2.RandomVerticalFlip(),
-        v2.RandomRotation(90),
-        v2.RandomAffine(0, translate=(0.1, 0.1)),
+def torh_train_transform(size=(224, 224), scale=(0.08, 1.0), rotation=30, flip=0.5):
+    image_transforms = v2.Compose([
+        # v2.ToImage(),
+        v2.RandomCrop(size),  # Random crop of size 256x256
+        # v2.Resize(size),
+        v2.RandomRotation(degrees=rotation, interpolation=InterpolationMode.BILINEAR),  # Random rotation within 30 degrees
+        # v2.RandomResize(size=size, scale=scale),  # Random rescale and crop
+        v2.RandomAffine(degrees=15, translate=(0.1, 0.1)),  # Random affine transformation
+        v2.RandomHorizontalFlip(p=flip),  # Random horizontal flip
+        v2.GaussianBlur(kernel_size=3),  # Apply Gaussian blur
+        # v2.RandomErasing(p=0.5),  # Random cutout
+        v2.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.1),  # Random color jitter
         v2.ToDtype(torch.float32, scale=True),
-        v2.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
+        v2.ToTensor(),
+        v2.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
     ])
 
-    return tv_transform
+    return image_transforms
 
-def albumentations_transform_valid(IMG_SIZE):
-    al_transform = A.Compose([
-        A.Resize(IMG_SIZE[0], IMG_SIZE[1]),
-        A.ShiftScaleRotate(p=0.5),
-        A.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
-        # ToTensorV2(),
+def torh_val_transform(size=(224, 224)):
+    image_transforms = v2.Compose([
+        v2.Resize(size),
+        v2.PILToTensor(),
+        v2.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
     ])
 
-    return al_transform
-
-def torch_transform_valid(IMG_SIZE):
-    tv_transform = v2.Compose([
-        v2.ToImage(),
-        v2.Resize(IMG_SIZE),
-        v2.ToDtype(torch.float32, scale=True),
-        v2.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
-    ])
-
-    return tv_transform
+    return image_transforms
 
 if __name__ == '__main__':
     config = {
@@ -65,8 +47,17 @@ if __name__ == '__main__':
         "image_size": (224, 224),
     }
 
-    transform = albumentations_transform_train(config["image_size"])
+    transform = torh_train_transform(size=config["image_size"])
 
     dataset = utils.GDXrayDataset(config, labels=True, transform=transform)
 
-    utils.visualize_samples(dataset, num_samples=3, labels=True)
+    img, lbl = dataset[0]
+
+    # utils.visualize_samples(dataset, 5)
+    utils.visualize_augmentations(dataset, 2, samples=2, cols=1)
+
+    # dataloader = torch.utils.data.DataLoader(dataset, batch_size=8, shuffle=True)
+
+    # for idx, img_mask in enumerate(dataloader):
+    #     print(img_mask[0].shape, img_mask[1].shape)
+    #     break
